4365756ceabf6d21a05bc6f5d45baf48c1a80b41
yawn <69970183+yawn-c111@users.noreply.github.com>
2022-11-04 12:52:19 +0900

    wip: getMyProjectCheerList

--- packages/contract/test/CheerListData.ts
+++ packages/contract/test/CheerListData.ts
@@ -52,7 +52,7 @@ describe('CheerListData', function () {
       getMyPoolCheerDataList= await cheerListData.getMyPoolCheerDataList(user1.address);
       expect(getMyPoolCheerDataList).to.deep.equal([]);
 
-      getMyProjectCheerList = await cheerListData.getMyProjectCheerList(user1.address);
+      getMyProjectCheerList = await cheerListData.getMyProjectCheerList(project1.address);
       expect(getMyProjectCheerList).to.deep.equal([]);
 
       addCheerDataList = await cheerListData.addCheerDataList(project1.address, userPool1.address, 123, "ガンバ！", 100);
@@ -64,30 +64,30 @@ describe('CheerListData', function () {
       expect(getMyPoolCheerDataList[0][3]).to.equal("ガンバ！");
       expect(getMyPoolCheerDataList[0][4]).to.equal(100);
 
-      const getMyProjectCheerList2 = await cheerListData.getMyProjectCheerList(userPool1.address);
-      expect(getMyProjectCheerList2[0][0]).to.equal(project1.address);
-      expect(getMyProjectCheerList2[0][1]).to.equal(userPool1.address);
-      expect(getMyProjectCheerList2[0][2]).to.equal(123);
-      expect(getMyProjectCheerList2[0][3]).to.equal("ガンバ！");
-      expect(getMyProjectCheerList2[0][4]).to.equal(100);
+      getMyProjectCheerList = await cheerListData.getMyProjectCheerList(userPool1.address);
+      expect(getMyProjectCheerList[0][0]).to.equal(project1.address);
+      expect(getMyProjectCheerList[0][1]).to.equal(userPool1.address);
+      expect(getMyProjectCheerList[0][2]).to.equal(123);
+      expect(getMyProjectCheerList[0][3]).to.equal("ガンバ！");
+      expect(getMyProjectCheerList[0][4]).to.equal(100);
 
 
 
-    //   addCheerDataList = await cheerListData.addCheerDataList(project2.address, userPool2.address, 456, "Foo", 200);
+      addCheerDataList = await cheerListData.addCheerDataList(project2.address, userPool2.address, 456, "Foo", 200);
 
-    //   getMyPoolCheerDataList = await cheerListData.getMyPoolCheerDataList(userPool2.address);
-    //   expect(getMyPoolCheerDataList[0][0]).to.equal(project2.address);
-    //   expect(getMyPoolCheerDataList[0][1]).to.equal(userPool2.address);
-    //   expect(getMyPoolCheerDataList[0][2]).to.equal(456);
-    //   expect(getMyPoolCheerDataList[0][3]).to.equal("Foo");
-    //   expect(getMyPoolCheerDataList[0][4]).to.equal(200);
+      getMyPoolCheerDataList = await cheerListData.getMyPoolCheerDataList(userPool2.address);
+      expect(getMyPoolCheerDataList[0][0]).to.equal(project2.address);
+      expect(getMyPoolCheerDataList[0][1]).to.equal(userPool2.address);
+      expect(getMyPoolCheerDataList[0][2]).to.equal(456);
+      expect(getMyPoolCheerDataList[0][3]).to.equal("Foo");
+      expect(getMyPoolCheerDataList[0][4]).to.equal(200);
 
-    //   getMyProjectCheerList = await cheerListData.getMyProjectCheerList(userPool2.address);
-    //   expect(getMyProjectCheerList[0][0]).to.equal(project2.address);
-    //   expect(getMyProjectCheerList[0][1]).to.equal(userPool2.address);
-    //   expect(getMyProjectCheerList[0][2]).to.equal(456);
-    //   expect(getMyProjectCheerList[0][3]).to.equal("Foo");
-    //   expect(getMyProjectCheerList[0][4]).to.equal(200);
+      getMyProjectCheerList = await cheerListData.getMyProjectCheerList(userPool2.address);
+      expect(getMyProjectCheerList[0][0]).to.equal(project2.address);
+      expect(getMyProjectCheerList[0][1]).to.equal(userPool2.address);
+      expect(getMyProjectCheerList[0][2]).to.equal(456);
+      expect(getMyProjectCheerList[0][3]).to.equal("Foo");
+      expect(getMyProjectCheerList[0][4]).to.equal(200);
     
     });
   });
